74a05e55d282ddd86fe2ead2a07a9733a9ab63cf
adurward <ajdurward@gmail.com>
2022-03-19 07:07:26 -0500

    Escape room continuing to finish

--- Escaperoom/escaperoom.js
+++ Escaperoom/escaperoom.js
@@ -1,9 +1,33 @@
 const readline = require("readline-sync");
 const name = readline.question("What is your name? ")
 
-var welcomeMessage = "${playerName}, welcome to the room you must eacape to survive!";
+var welcomeMessage = "${name}, welcome to the room you must eacape to survive!";
 console.log(welcomeMessage);
 
 var isAlive = true;
 var hasKey = false;
 
+while(isAlive == true){
+    const action = readline.keyIn("${name}, there are three things you can do to get out of this situation, [p] put your hand in the hole, [f] find the key, [o] open the door", {limit: "pfo"})
+
+    if (action = "p") {
+        put()
+    } else if (action = "f") {
+        find()
+    } else if (action = "o") {
+        open()
+    }
+}
+
+function put() {
+    var random = Math.ceil(Math.random()*2)
+    if(random === 1) {
+        youAreDead()
+    } else {
+        console.log("Bugs, I hate bugs! Decide last minute to pull your hand away")
+    }
+}
+
+function find() {
+    hasKey = true
+}
